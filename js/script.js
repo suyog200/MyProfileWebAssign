fd2b5085abe7b0a2824002b0ada3181ed26dece2
suyog200 <suygchari@gmail.com>
2023-02-17 20:23:23 +0530

    added smooth scroll reveal and fixed some issues

--- js/script.js
+++ js/script.js
@@ -44,3 +44,33 @@ function scrollActive() {
     }
   });
 }
+/*====scroll animation===*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal('.home__title', {});
+sr.reveal('.home__img', { origin: 'right', delay: 400 });
+sr.reveal('.about__img', { delay: 500 });
+sr.reveal('.about__subtitle', { delay: 300 });
+sr.reveal('.about__profession', { delay: 400 });
+sr.reveal('.about__text', { delay: 500 });
+sr.reveal('.about__social-icon', { delay: 600, interval: 200 });
+sr.reveal('.education__subtitle', { delay: 300 });
+sr.reveal('.education__text', { delay: 500 });
+
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 });
+sr.reveal('.skills__img', { delay: 400 });
+
+sr.reveal('.experience__subtitle', { delay: 500 });
+sr.reveal('.experience__text', { delay: 400 });
+sr.reveal('.experience__link', { delay: 300 });
+
+sr.reveal('.project__img', { interval: 200 });
+
+sr.reveal('.contact__subtitle', {});
+sr.reveal('.contact__text', { interval: 200 });
